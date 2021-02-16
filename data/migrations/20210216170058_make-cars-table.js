f26676c2ff2bd62157876c63a5a40c80933b5ca8
Nathan Hietala <Nrhietala@gmail.com>
2021-02-16 17:42:32 -0600

    Migrate/seed database succesfully

--- data/migrations/20210216170058_make-cars-table.js
+++ data/migrations/20210216170058_make-cars-table.js
@@ -1,16 +1,20 @@
 exports.up = function (knex) {
   // make the cars table
   return knex.schema.createTable("cars", table => {
-    // add primary key unique, not null 'id' (integer)
     table.increments("car_id");
+
     // add not null "vin" (integer)
     table.integer("vin").notNullable().unique();
+
     // add not null "make" (string)
     table.string("make", 128).notNullable();
+
     // add not null "model" (string)
     table.string("model", 128).notNullable();
+
     // add not null "milage" (integer)
     table.integer("mileage").notNullable();
+
     // add optional transmission type (string)
     table.string("type_transmission", 128);
   });
